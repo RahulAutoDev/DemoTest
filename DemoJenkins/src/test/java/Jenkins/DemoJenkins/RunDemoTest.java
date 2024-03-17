b49aa78c926088842b34b34dddcb9126cc8b0f14
Rahul Singh <64746581+RahulAutoDev@users.noreply.github.com>
2024-03-17 12:42:12 +0530

    Assert Fail added

--- DemoJenkins/src/test/java/Jenkins/DemoJenkins/RunDemoTest.java
+++ DemoJenkins/src/test/java/Jenkins/DemoJenkins/RunDemoTest.java
@@ -2,6 +2,7 @@ package Jenkins.DemoJenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,6 +16,7 @@ public class RunDemoTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		System.out.println(driver.getCurrentUrl());
+		Assert.assertEquals(driver.getCurrentUrl(), "Testdata");
 		System.out.println("Hello JENKINS.....");
 		driver.quit();
 	}
