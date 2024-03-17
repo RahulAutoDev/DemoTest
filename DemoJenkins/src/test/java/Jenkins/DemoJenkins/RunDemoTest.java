46b06528e5f09f8b4855d833943c0833ebce262a
Rahul Singh <64746581+RahulAutoDev@users.noreply.github.com>
2024-03-17 12:12:08 +0530

    First Commit

---
+++ DemoJenkins/src/test/java/Jenkins/DemoJenkins/RunDemoTest.java
@@ -0,0 +1,24 @@
+package Jenkins.DemoJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RunDemoTest {
+
+	@Test
+	public void runMyTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getCurrentUrl());
+		System.out.println("Hello JENKINS.....");
+		driver.quit();
+	}
+	
+	
+
+}
