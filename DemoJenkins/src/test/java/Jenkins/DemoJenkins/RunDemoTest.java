335897a2f3616c73380f1def366425e530af00c8
Rahul Singh <64746581+RahulAutoDev@users.noreply.github.com>
2024-03-19 10:25:56 +0530

    Relative Locator added

--- DemoJenkins/src/test/java/Jenkins/DemoJenkins/RunDemoTest.java
+++ DemoJenkins/src/test/java/Jenkins/DemoJenkins/RunDemoTest.java
@@ -1,7 +1,10 @@
 package Jenkins.DemoJenkins;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,11 +13,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class RunDemoTest {
 
 	@Test
-	public void runMyTest()
+	public void runMyTest() throws InterruptedException
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
+		WebElement googleSearchBar = driver.findElement(By.cssSelector("#APjFqb"));
+		WebElement googleFeelBtn = driver.findElement(RelativeLocator.with(By.tagName("input")).below(googleSearchBar));
+		googleFeelBtn.click();
+		Thread.sleep(5000);
 		System.out.println(driver.getCurrentUrl());
 		//Assert.assertEquals(driver.getCurrentUrl(), "Testdata");
 		System.out.println("Hello JENKINS.....");
